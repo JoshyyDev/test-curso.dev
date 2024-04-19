70efee06f6f60702b59133a5f59692d591f080ae
Josué Cortez <87339261+JoshyyDev@users.noreply.github.com>
2024-04-19 01:40:15 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>PALMEIRAS TEM MUNDIAL</h1>
+  return <h1>PALMEIRAS TEM MUNDIAL</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("errado");
 }
 
 export default Home;
