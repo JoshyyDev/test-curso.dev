d48ab8660d8b568424ce5361da98ca3f9dcc5c1a
Josué Cortez <87339261+JoshyyDev@users.noreply.github.com>
2024-04-19 01:43:14 +0000

    tirando codigo console.log

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>PALMEIRAS TEM MUNDIAL</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("errado");
-}
-
 export default Home;
